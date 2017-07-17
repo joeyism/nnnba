13b0c670c24a44c05b2dfd403fd063b7b34292ba
Joey Sham <sham.joey@gmail.com>
2017-07-17 16:02:49 -0400

    Added nba_player class

--- nba_funcs.py
+++ nba_funcs.py
@@ -4,15 +4,38 @@ from nba_py import player
 from lxml import html
 import re
 
+class NBA_player:
 
-def getPlayerStats(player_id):
-    yoy = player.PlayerYearOverYearSplits(measure_type="Base", player_id=player_id)
-    json = yoy.json
-    total = []
-    for yeardata in json["resultSets"][0]["rowSet"]:
-        total.append(yeardata[5:])
+    def __init__(self, player_id, name_reverse, name):
+        self.player_id = player_id
+        self.name_reverse = name_reverse
+        self.name = name
 
-    return total
+    def getPlayerStats(self):
+        yoy = player.PlayerYearOverYearSplits(measure_type="Base", player_id=self.player_id)
+        json = yoy.json
+        total = []
+        for yeardata in json["resultSets"][0]["rowSet"]:
+            total.append(yeardata[5:])
+    
+        return total
+    
+    def __parseSalaryText(rawSalary):
+        textSalary = re.sub('\s+', '', rawSalary)
+        return int(textSalary[1:].replace(",",""))
+    
+    def getProjectedSalary(self):
+        playerName = "-".join(playerName.replace(".","").split(" "))
+        url = "http://hoopshype.com/player/{}/salary/".format(self.name)
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        salaryParents = tree.xpath('//*[@id="content"]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr')
+        salaries = []
+        for salaryParent in salaryParents:
+            thisSalary = self.__parseSalaryText(salaryParent.getchildren()[1].text)
+            salaries.append(thisSalary)
+        return salaries
+
 
 def getAllPlayers():
     playerlist = nba_py.player.PlayerList()
@@ -23,18 +46,3 @@ def getAllPlayers():
         total.append(player[0:3])
     return total
 
-def parseSalaryText(rawSalary):
-    textSalary = re.sub('\s+', '', rawSalary)
-    return int(textSalary[1:].replace(",",""))
-
-def getProjectedSalary(playerName):
-    playerName = "-".join(playerName.replace(".","").split(" "))
-    url = "http://hoopshype.com/player/{}/salary/".format(playerName)
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-    salaryParents = tree.xpath('//*[@id="content"]/div[2]/div[3]/div[1]/div[1]/table/tbody/tr')
-    salaries = []
-    for salaryParent in salaryParents:
-        thisSalary = parseSalaryText(salaryParent.getchildren()[1].text)
-        salaries.append(thisSalary)
-    return salaries
